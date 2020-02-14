b5d1d2823482a5eb810b43da3133f97a64265d3d
Shane Stacy <43557379+TRSlimShaney@users.noreply.github.com>
2020-02-14 12:49:47 -0500

    Add files via upload

---
+++ CMax&Avg/avg.c
@@ -0,0 +1,32 @@
+/*
+Author:         Shane Stacy
+Description:    This program averages the last three integers, returned as unsigned integer (rounded down)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int array[3];
+int counter = 0;
+
+
+unsigned int avg(unsigned int num) {
+    if (counter == 0) {
+        array[0] = num;
+    }
+    else if (counter == 1) {
+        array[1] = num;
+    }
+    else if (counter == 2) {
+        array[2] = num;
+    }
+
+    counter++;
+
+    if (counter == 3) {
+        counter = 0;
+    }
+
+    return ((array[0]/3) + (array[1]/3) + (array[2]/3));
+}
+
